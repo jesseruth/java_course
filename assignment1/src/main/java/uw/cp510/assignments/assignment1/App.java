c7906e6503ddbe9e8d18b00c805e10ece7861abc
Jesse Ruth <jruth21@uw.edu>
2021-10-06 13:18:26 -0700

    mvn initial run

---
+++ assignment1/src/main/java/uw/cp510/assignments/assignment1/App.java
@@ -0,0 +1,13 @@
+package uw.cp510.assignments.assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
