524cd27e226e51e17e6c04cd287e794cfd0a6a27
Jesse Ruth <jesse.ruth@expeditors.com>
2021-10-11 09:21:56 -0700

    added bonus

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,1 +1,1 @@
-java -cp /home/laptop/src/java_course/assignment1/target/classes uw.cp510.assignments.assignment1.Figures
+java -cp target/classes uw.cp510.assignments.assignment1.Figures
--- assignment1/src/main/java/uw/cp510/assignments/assignment1/Figures.java
+++ assignment1/src/main/java/uw/cp510/assignments/assignment1/Figures.java
@@ -2,49 +2,59 @@ package uw.cp510.assignments.assignment1;
 
 /**
  * CP510 Assignment 1
- *
+ * <p>
  * 1. Create a Maven project
  * 2. Import the project into your IDEa
  * 3. Add this file to your project
  * 4. Complete the methods rectPerimeter, rectArea, circPerimeter and circArea.
  * 5. For an extra 10 points, complete rectPerimeterDiag and rectAreaDiag.
  */
-public class Figures
-{
+public class Figures {
     /**
      * Entry point.
      *
      * @param args command-line arguments, not used.
      */
-    public static void main(String[] args)
-    {
-        double[][]  rects   = { { 5, 10 }, { 15, 6 }, { 7, 19 } };
-        String      fmt     = "\tside1: %10.5f, side2: %10.5f, "
+    public static void main(String[] args) {
+        double[][] rects = {{5, 10}, {15, 6}, {7, 19}};
+        String fmt = "\tside1: %10.5f, side2: %10.5f, "
                 + "peri = %10.5f, area = %10.5f%n";
-        System.out.println( "Rectangles" );
-        for ( double[] rect : rects )
-        {
-            double  side1   = rect[0];
-            double  side2   = rect[1];
-            double  peri    = rectPerimeter( side1, side2 );
-            double  area    = rectArea( side1, side2 );
-            System.out.printf( fmt, side1, side2, peri, area );
+        System.out.println("Rectangles");
+        for (double[] rect : rects) {
+            double side1 = rect[0];
+            double side2 = rect[1];
+            double peri = rectPerimeter(side1, side2);
+            double area = rectArea(side1, side2);
+            System.out.printf(fmt, side1, side2, peri, area);
         }
 
-        double[]    circs   = { 5, 10, 15 };
+        double[] circs = {5, 10, 15};
         fmt = "\tradius: %10.5f, peri = %10.5f, area = %10.5f%n";
-        System.out.println( "Circles" );
-        for ( double radius : circs )
-        {
-            double   peri    = circPerimeter( radius );
-            double   area    = circArea( radius );
-            System.out.printf( fmt, radius, peri, area );
+        System.out.println("Circles");
+        for (double radius : circs) {
+            double peri = circPerimeter(radius);
+            double area = circArea(radius);
+            System.out.printf(fmt, radius, peri, area);
         }
 
-        double rect = rectPerimeterDiag(5, 2);
-        System.out.println( "rect" );
-        fmt = "\trect: %10.5f";
-        System.out.printf( fmt, rect);
+        System.out.println("Bonus perimeter");
+        double[][] diagonals = {{5, 1}, {15, 2}, {7, 3}};
+        fmt = "\tdiag: %10.5f, slope = %10.5f, perimeter = %10.5f%n";
+        for (double[] diag : diagonals) {
+            double diagonal = diag[0];
+            double slope = diag[1];
+            double rect = rectPerimeterDiag(diagonal, slope);
+            System.out.printf(fmt, diagonal, slope, rect);
+        }
+
+        System.out.println("Bonus Area");
+        fmt = "\tdiag: %10.5f, slope = %10.5f, area = %10.5f%n";
+        for (double[] diag : diagonals) {
+            double diagonal = diag[0];
+            double slope = diag[1];
+            double rect = rectAreaDiag(diagonal, slope);
+            System.out.printf(fmt, diagonal, slope, rect);
+        }
     }
 
     /**
@@ -53,11 +63,10 @@ public class Figures
      * @param side1 the first side; must be greater than 0
      * @param side2 the adjacent side; must be greater than 0
      * @return the perimeter of the rectangle, or Double.NaN
-     *         if either parameter is invalid
+     * if either parameter is invalid
      */
-    public static double rectPerimeter( double side1, double side2 )
-    {
-        double  peri = (2.0 * side1) + (2.0 * side2); // replace the 0 with the perimeter.
+    public static double rectPerimeter(double side1, double side2) {
+        double peri = (2.0 * side1) + (2.0 * side2); // replace the 0 with the perimeter.
         return peri;
     }
 
@@ -67,27 +76,24 @@ public class Figures
      * @param side1 the first side; must be greater than 0
      * @param side2 the adjacent side; must be greater than 0
      * @return the area of the rectangle, or Double.NaN
-     *         if either parameter is invalid
+     * if either parameter is invalid
      */
-    public static double rectArea( double side1, double side2 )
-    {
-        double area =  side1 * side2; // replace the 0 with the area.
+    public static double rectArea(double side1, double side2) {
+        double area = side1 * side2; // replace the 0 with the area.
         return area;
     }
 
     /**
      * Given the radius of a circle, calculate the perimeter of the circle.
      *
-     * @param radius    the given radius
-     * @return the perimeter of the circle
-     *
+     * @param radius the given radius
      * @param radius the radius; must be greater than 0
+     * @return the perimeter of the circle
      * @return the perimeter of the circle, or Double.NaN
-     *         if the parameter is invalid
+     * if the parameter is invalid
      */
-    public static double circPerimeter( double radius )
-    {
-        double  peri = 2.0 * Math.PI * radius; // replace the 0 with the perimeter.
+    public static double circPerimeter(double radius) {
+        double peri = 2.0 * Math.PI * radius; // replace the 0 with the perimeter.
         return peri;
     }
 
@@ -96,11 +102,10 @@ public class Figures
      *
      * @param radius the radius of the circle; must be greater than 0
      * @return the area of the circle, or Double.NaN
-     *         if the parameter is invalid
+     * if the parameter is invalid
      */
-    public static double circArea( double radius )
-    {
-        double area =  Math.PI * radius * radius; // replace the 0 with the area.
+    public static double circArea(double radius) {
+        double area = Math.PI * radius * radius; // replace the 0 with the area.
         return area;
     }
 
@@ -112,13 +117,12 @@ public class Figures
      * Given the length and slope of the diagonal of a rectangle,
      * compute the perimeter of the rectangle.
      *
-     * @param diag the given length; must be greater than 0
+     * @param diag  the given length; must be greater than 0
      * @param slope the given slope; must be greater than 0
      * @return the perimeter of the rectangle, or Double.NaN
-     *         if either parameter is invalid
+     * if either parameter is invalid
      */
-    public static double rectPerimeterDiag( double diag, double slope )
-    {
+    public static double rectPerimeterDiag(double diag, double slope) {
         // a = b * slope
         // a^2 + b^2 = c^2
         // b = c /
@@ -129,8 +133,7 @@ public class Figures
 
         double b = diag / Math.sqrt(((slope * slope) + 1));
         double a = b * slope;
-        System.out.println( "rect, a:" + a + ", b: " + b + "c: " + diag );
-        double peri  =  b + a + diag; // replace the 0 with the perimeter.
+        double peri = 2 * b + 2 * a; // replace the 0 with the perimeter.
         return peri;
     }
 
@@ -138,14 +141,20 @@ public class Figures
      * Given the length and slope of the diagonal of a rectangle,
      * compute the area of the rectangle.
      *
-     * @param diag the given length
+     * @param diag  the given length
      * @param slope the given slope
      * @return the area of the rectangle
      */
     // Bonus: for an extra 10 points, complete the following methods.
-    public static double rectAreaDiag( double diag, double slope )
-    {
-        double area     =  0; // replace the 0 with the area.
+    public static double rectAreaDiag(double diag, double slope) {
+
+        if (diag < 0 || slope < 0) {
+            return Double.NaN;
+        }
+        double b = diag / Math.sqrt(((slope * slope) + 1));
+        double a = b * slope;
+
+        double area = a * b; // replace the 0 with the area.
         return area;
     }
 }
