94a83ae2a01adaf642e03fe585f62d2957eeacb5
Madongo <madongo@gmail.com>
2021-12-04 06:08:31 -0800

    Add test for Dlist

---
+++ assignment7/src/test/java/cp120/DListSimpleTest.java
@@ -0,0 +1,71 @@
+package cp120;
+
+import cp510.d_list.DList;
+import cp510.d_list.DNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DListSimpleTest
+{
+    @Test
+    public void addTailTest()
+    {
+        // Create an empty list. Verify that list.flink and list.blink -> list
+        DList list    = new DList();
+        assertEquals( list, list.getNext() );
+        assertEquals( list, list.getPrevious() );
+        assertEquals( list, list.getHead() );
+        assertEquals( list, list.getTail() );
+        assertEquals( 0, list.size() );
+        assertTrue( list.isEmpty() );
+
+        // The first node added to the list will be both head and tail
+        /*
+         *        List root       head/tail
+         *              +---+      +---+
+         *              |   |--->  |   |---> to root
+         *              +---+      +---+
+         * to head <--- |   |  <---|   |
+         *              +---+      +---+
+         */
+        DNode   head    = new DNode( 0 );
+        list.addTail( head );
+        assertEquals( list, head.getNext() );
+        assertEquals( list, head.getPrevious() );
+        assertEquals( head, list.getNext() );
+        assertEquals( head, list.getPrevious() );
+        assertEquals( head, list.getHead() );
+        assertEquals( head, list.getTail() );
+        assertEquals( 1, list.size() );
+        assertFalse( list.isEmpty() );
+
+
+        // Slowly build list, performing validations with each add
+        for ( int inx = 1 ; inx <= 10 ; ++inx )
+        {
+            DNode   node    = new DNode( inx );
+            list.addTail( node );
+
+            int     expSize = inx + 1;
+
+            assertNotEquals( list, head.getNext() );
+            assertNotEquals( list, node.getPrevious() );
+            assertEquals( list, head.getPrevious() );
+            assertEquals( list, node.getNext() );
+            assertEquals( head, list.getHead() );
+            assertEquals( node, list.getTail() );
+            assertEquals( head, list.getNext() );
+            assertEquals( node, list.getPrevious() );
+            assertEquals( expSize, list.size() );
+            assertFalse( list.isEmpty() );
+        }
+
+        DNode node    = list.getHead();
+        while ( node != list )
+        {
+            System.out.println( node.getData() );
+            node = node.getNext();
+        }
+    }
+}
